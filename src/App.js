bbd343dc6293092d0405af29c90c74de4d085fec
kumomiki <rita21625@gmail.com>
2023-03-18 18:04:21 -0500

    feat: add input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Button } from './components/basic_component/button';
 import style from 'src/App.module.scss'
+import { Input } from './components/basic_component/input';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className={style.appContainer}>
         <p>
           GtiHub Test
         </p>
         <Button title='推文' className={style.buttonOrange}/>
         <Button title='推文' className={style.buttonWhite}/>
+        <Input />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/basic_component/input.jsx
@@ -0,0 +1,9 @@
+import style from 'src/components/basic_component/input.module.scss'
+
+export const Input = () => {
+  return (
+    <div className={style.inputContainer}>
+      <input type="text" />
+    </div>
+  )
+}
