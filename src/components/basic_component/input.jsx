3e4bfb1818af52aadeca4eb04b8dbcd4b46284b6
kumomiki <rita21625@gmail.com>
2023-03-19 17:14:14 -0500

    feat: add the other input component

--- src/components/basic_component/input.jsx
+++ src/components/basic_component/input.jsx
@@ -3,7 +3,7 @@ import style from 'src/components/basic_component/input.module.scss'
 export const Input = ({title}) => {
   return (
     <div className={style.inputContainer}>
-      <div className={style.inputTitle}>標題</div>
+      <div className={style.inputTitle}>{title}</div>
       <input type="text" placeholder="請輸入帳號" />
     </div>
   );
