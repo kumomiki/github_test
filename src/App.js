755b6f5b548384fa9c6360043cce450df295f1ae
Ollieeryo <ollieeryo@gmail.com>
2023-03-19 18:23:51 +0800

    feature: add Form component 請確認 PR

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Button } from './components/basic_component/button';
 import style from 'src/App.module.scss'
 import { Input } from './components/basic_component/input';
 import { Hello } from './components/basic_component/Hello'
+import Form from './components/basic_component/Form';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Button title='推文' className={style.buttonWhite}/>
         <Input />
         <Hello />
+        <Form />
         </div>
       </header>
     </div>
---
+++ src/components/basic_component/Form.jsx
@@ -0,0 +1,12 @@
+function Form() {
+  return (
+    <div>
+      <form action="">
+        <p>請輸入你的名字</p>
+        <input type="text" placeholder="please type your name" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
